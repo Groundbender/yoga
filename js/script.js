c10da2caa6444d061515c391a8da24680a5029ba
Damir <damir24may@mail.ru>
2023-03-06 02:36:11 +0300

    added send form with promise

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ const tabContent = document.querySelectorAll(".info-tabcontent");
 
 const hideTabContent = () => {
   tabContent.forEach((content, index) => {
-    console.log(tabContent[0]);
     if (index != 0) {
       content.classList.remove("show");
       content.classList.add("hide");
@@ -76,7 +75,7 @@ const setCLock = (id, endtime) => {
   timeInterval = setInterval(updateCLock, 1000);
   updateCLock();
 };
-setCLock("timer", "2023-03-5");
+setCLock("timer", "2023-03-4");
 
 // modal
 
@@ -108,3 +107,133 @@ document.addEventListener("keyup", (event) => {
     document.body.style.overflow = "";
   }
 });
+
+// anchors
+
+const header = document.querySelector("header>.container");
+const links = header.querySelectorAll("ul>li>a");
+
+console.log(header);
+console.log(links);
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    let blockID = link.getAttribute("href");
+    document.querySelector("" + blockID).scrollIntoView({
+      block: "start",
+      behavior: "smooth",
+    });
+  });
+});
+
+// sendForm
+// let message = {
+//   loading: "Загрузка...",
+//   success: "Спасибо, скоро мы с вами свяжемся!",
+//   failure: "Что-то пошло не так...",
+// };
+
+// let form = document.querySelector(".main-form");
+// let input = form.getElementsByTagName("input");
+
+// let statusMessage = document.createElement("div");
+
+// statusMessage.classList.add("status");
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   form.appendChild(statusMessage);
+
+//   let request = new XMLHttpRequest();
+
+//   request.open("POST", "server.php");
+//   request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+
+//   let formData = new FormData(form);
+//   let obj = {};
+
+//   formData.forEach(function (value, key) {
+//     obj[key] = value;
+//   });
+
+//   let json = JSON.stringify(obj);
+
+//   request.send(json);
+
+//   request.addEventListener("readystatechange", function () {
+//     if (request.readyState < 4) {
+//       statusMessage.innerHTML = message.loading;
+//     } else if (request.readyState === 4 && request.status == 200) {
+//       statusMessage.innerHTML = message.success;
+//     } else {
+//       statusMessage.innerHTML = message.failure;
+//     }
+//   });
+
+//   for (let i = 0; i < input.length; i++) {
+//     input[i].value = "";
+//   }
+// });
+
+let message = {
+  loading: "Загрузка...",
+  success: "Спасибо, скоро мы с вами свяжемся!",
+  failure: "Что-то пошло не так...",
+};
+
+let form = document.querySelector(".main-form");
+let formBottom = document.querySelector("#form");
+let input = form.getElementsByTagName("input");
+
+let statusMessage = document.createElement("div");
+
+statusMessage.classList.add("status");
+
+function sendForm(elem) {
+  elem.addEventListener("submit", function (e) {
+    e.preventDefault();
+    elem.appendChild(statusMessage);
+    let formData = new FormData(elem);
+
+    function postData(data) {
+      return new Promise(function (resolve, reject) {
+        let request = new XMLHttpRequest();
+        request.open("POST", "server.php");
+        request.setRequestHeader(
+          "Content-type",
+          "application/json; charset=utf-8"
+        );
+
+        request.onreadystatechange = function () {
+          if (request.readyState < 4) {
+            resolve();
+          } else if (request.readyState === 4) {
+            if (request.status == 200 && request.status < 300) {
+              resolve();
+            } else {
+              reject();
+            }
+          }
+        };
+        request.send(data);
+      });
+    }
+    function clearInput() {
+      for (let i = 0; i < input.length; i++) {
+        input[i].value = "";
+      }
+    }
+    postData(formData)
+      .then(() => (statusMessage.innerHTMl = message.loading))
+      .then(() => {
+        thanksModal.style.display = "block";
+        mainModal.style.display = "none";
+        statusMessage.innerHTML = "";
+      })
+      .catch(() => (statusMessage.innerHTML = message.failure))
+      .then(clearInput);
+  });
+}
+sendForm(form);
+sendForm(formBottom);
